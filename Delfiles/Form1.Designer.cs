355c6924cddb1214229a1b5407b8213c938b37f1
sabina aqashova <aqashova@inbox.ru>
2018-12-21 22:32:05 +0400

    FC

---
+++ Delfiles/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Delfiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnchs = new System.Windows.Forms.Button();
+            this.btndel = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnchs
+            // 
+            this.btnchs.Location = new System.Drawing.Point(322, 115);
+            this.btnchs.Name = "btnchs";
+            this.btnchs.Size = new System.Drawing.Size(75, 23);
+            this.btnchs.TabIndex = 0;
+            this.btnchs.Text = "Choose Folder";
+            this.btnchs.UseVisualStyleBackColor = true;
+            this.btnchs.Click += new System.EventHandler(this.btnchs_Click);
+            // 
+            // btndel
+            // 
+            this.btndel.Location = new System.Drawing.Point(322, 160);
+            this.btndel.Name = "btndel";
+            this.btndel.Size = new System.Drawing.Size(75, 23);
+            this.btndel.TabIndex = 1;
+            this.btndel.Text = "Delete";
+            this.btndel.UseVisualStyleBackColor = true;
+            this.btndel.Click += new System.EventHandler(this.btndel_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(449, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btndel);
+            this.Controls.Add(this.btnchs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnchs;
+        private System.Windows.Forms.Button btndel;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Delfiles/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Delfiles
+{
+    public partial class Form1 : Form
+    {
+        
+        private string[] files;
+        private string[] folders;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnchs_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+            folders = Directory.GetDirectories(folderBrowserDialog1.SelectedPath);
+            foreach (var file in files)
+            {
+                FileInfo info = new FileInfo(file);
+                // info1 = info;
+
+                if (info.LastAccessTime <= DateTime.Now.AddMonths(-3))
+                {
+                    listBox1.Items.Add(info.Name);
+                    MessageBox.Show(info.LastAccessTime.ToString());
+                }
+                else
+                {
+                    MessageBox.Show(info.Name + " " + " is new");
+                }
+            }
+            foreach (var folder in folders)
+            {
+                DirectoryInfo infofolder = new DirectoryInfo(folder);
+                if (infofolder.LastAccessTime <= DateTime.Now.AddMonths(-3))
+                {
+                    listBox1.Items.Add(infofolder.Name);
+                    MessageBox.Show(infofolder.LastAccessTime.ToString());
+                }
+                else
+                {
+                    MessageBox.Show(infofolder.Name + " " + " is new");
+                }
+
+            }
+        }
+
+        private void btndel_Click(object sender, EventArgs e)
+        {
+            foreach (var file in files)
+            {
+                FileInfo info1 = new FileInfo(file);
+                if (info1.LastAccessTime <= DateTime.Now.AddMonths(-3))
+                {
+                    info1.Delete();
+                }
+                else
+                {
+                    MessageBox.Show(info1.Name + " : " + " you can't delete this file");
+                }
+
+            }
+            foreach (var folder in folders)
+            {
+                DirectoryInfo infofolder1 = new DirectoryInfo(folder);
+                if (infofolder1.LastAccessTime <= DateTime.Now.AddMonths(-3))
+                {
+                    infofolder1.Delete();
+                }
+                else
+                {
+                    MessageBox.Show(infofolder1.Name + " : " + " you can't delete this folder");
+                }
+
+            }
+
+        }
+    }
+}
